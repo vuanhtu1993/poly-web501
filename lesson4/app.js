dc13ce3fc9fa01b2e68e937abbe87bf16942c279
tuva9 <tuva9@fpt.edu.vn>
2023-06-01 10:00:22 +0700

    imperative and declarative

---
+++ lesson4/app.js
@@ -0,0 +1,55 @@
+// const btnSwitch = document.createElement('button')
+// btnSwitch.innerText = "Switch"
+// const bulb = document.createElement('img')
+// bulb.src = "./images/on.png"
+// bulb.style.maxWidth = "100px"
+
+// document.body.appendChild(bulb)
+// document.body.appendChild(btnSwitch)
+
+// btnSwitch.addEventListener('click', function () {
+//     if (bulb.src == "http://127.0.0.1:5500/lesson4/images/on.png") {
+//         bulb.src = "./images/off3.png"
+//     } else {
+//         bulb.src = "./images/on.png"
+//     }
+
+// })
+
+// const component = {
+//     status: false,
+//     render: function () {
+//         return `
+//         <div>
+//         ${this.status
+//                 ? `<img width="100px" src="./images/on.png"/>`
+//                 : `<img width="100px" src="./images/off.png"/>`}
+//             <button id="switch">Switch</button>
+//         </div>
+//     `
+//     },
+//     afterRender: function () {
+//         console.log('xxxx');
+//         document.querySelector('#switch').onclick = function () {
+//             this.status = !this.status
+//             component.render()
+//         }
+//     }
+// }
+
+
+const render = function () {
+    let status = false
+    document.body.innerHTML = `<div>
+    ${status
+            ? `<img style="max-width: 100px" src="./images/on.png"/>`
+            : `<img style="max-width: 50px" src="./images/off.png"/>`}
+        <button id="switch">Switch</button>
+    </div>
+    `
+    document.querySelector('#switch').onclick = function () {
+        status = !status
+        render()
+    }
+}
+render()
