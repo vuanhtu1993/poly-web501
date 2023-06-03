2991faee20cde88b41d552b2d8b315855993d7f6
tuva9 <tuva9@fpt.edu.vn>
2023-06-03 10:11:56 +0700

    Buổi 12: Imperative and declarative

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,62 +1,33 @@
+// Imperative
+// const bulb = document.createElement('img')
+// bulb.src = "./images/off3.png"
 // const btnSwitch = document.createElement('button')
 // btnSwitch.innerText = "Switch"
-// const bulb = document.createElement('img')
-// bulb.src = "./images/on.png"
-// bulb.style.maxWidth = "100px"
 
-// document.body.appendChild(bulb)
-// document.body.appendChild(btnSwitch)
-
-// btnSwitch.addEventListener('click', function () {
-//     if (bulb.src == "http://127.0.0.1:5500/lesson4/images/on.png") {
-//         bulb.src = "./images/off3.png"
-//     } else {
+// btnSwitch.onclick = function () {
+//     if (bulb.src == "http://127.0.0.1:5500/lesson4/images/off3.png") {
 //         bulb.src = "./images/on.png"
-//     }
-
-// })
-
-// const component = {
-//     status: false,
-//     render: function () {
-//         return `
-//         <div>
-//         ${this.status
-//                 ? `<img width="100px" src="./images/on.png"/>`
-//                 : `<img width="100px" src="./images/off.png"/>`}
-//             <button id="switch">Switch</button>
-//         </div>
-//     `
-//     },
-//     afterRender: function () {
-//         console.log('xxxx');
-//         document.querySelector('#switch').onclick = function () {
-//             this.status = !this.status
-//             component.render()
-//         }
+//     } else {
+//         bulb.src = "./images/off3.png"
 //     }
 // }
 
-// const useState = function (initState) {
-//     let state = initState
-//     const setState = function (newState) {
-//         state = newState
-//     }
-//     return [state, setState]
-// }
+// document.body.append(bulb)
+// document.body.append(btnSwitch)
 
+// Declarative
 
-let status = false
-const render = function () {
-    document.body.innerHTML = `<div>
-    ${status
-            ? `<img style="max-width: 100px" src="./images/on.png"/>`
-            : `<img style="max-width: 50px" src="./images/off.png"/>`}
-        <button id="switch">Switch</button>
+let state = false
+
+function render() {
+    document.body.innerHTML = /*html*/`
+    <div>
+        <img width="100" src="${state ? './images/on.png' : './images/off3.png'}"/>
+        <button id="btnSwitch">Switch</button>
     </div>
     `
-    document.querySelector('#switch').onclick = function () {
-        status = !status
+    document.querySelector('#btnSwitch').onclick = function () {
+        state = !state
         render()
     }
 }
