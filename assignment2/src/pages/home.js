622a4bffa9cb9ab94692004b2c15c6934c82a090
tuva9 <tuva9@fpt.edu.vn>
2023-06-13 11:20:54 +0700

    Base project

--- assignment2/src/pages/home.js
+++
@@ -1,9 +0,0 @@
-const HomePage = function () {
-    return /*html*/`
-        <div>
-            <h1 class="bg-red-200">HomePage</h1>
-        </div>
-     `
-}
-
-export default HomePage
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '../style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 class="bg-red-500">Base project with Tailwind and Bootstrap</h1>
+    <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button class="btn btn-primary">Primary button</button>
+    </div>
+  </div>
+`
